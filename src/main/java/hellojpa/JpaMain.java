eee94460faf936df08e331bc283feb353be4c083
seung_ho_choi.s <74850409+chltmdgh522@users.noreply.github.com>
2024-02-04 21:27:20 +0900

    JpaMain.java 업데이트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class JpaMain {
 
- 
+  
    public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
