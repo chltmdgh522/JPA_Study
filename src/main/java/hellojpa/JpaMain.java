ff6a55d7e0e2884c830275d46174f5730f60eaa1
seung_ho_choi.s <chltmdgh517@naver.com>
2024-02-23 20:26:50 +0900

    양방향 연관관계와 연관관계의 주인1- 기본

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,8 @@ import hellojpa.section5.Member;
 import hellojpa.section5.Team;
 import jakarta.persistence.*;
 
+import java.util.List;
+
 
 public class JpaMain {
 
@@ -28,15 +30,15 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
+            em.flush();
+            em.clear();
             //조회
             Member findMember = em.find(Member.class, member.getId());
-            // Long findTemaId = findMember.getTeamId();
-            // Team findTeam= em.find(Team.class, findTeamId);
-            Team findTeam = findMember.getTeam();
-
-            //수정
-            Team newTeam = em.find(Team.class, 100L); //100번이 있다 가정하고
-            findMember.setTeam(newTeam);
+            List<Member> members = findMember.getTeam().getMembers();
+            System.out.println("===========");
+            for (Member member1 : members) {
+                System.out.println(member1.getUserName());
+            }
 
             tx.commit();
         } catch (Exception e) {
