86c2d95de9c334669db55f9018657b6a15e215c1
seung_ho_choi.s <chltmdgh517@naver.com>
2024-02-23 20:59:20 +0900

    양방향 연관관계와 연관관계의 주인 2 - 주의점, 정리

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,6 +18,7 @@ public class JpaMain {
         tx.begin();
 
         try {
+
             //팀 저장
             Team team = new Team();
             team.setName("TeamA");
@@ -26,20 +27,23 @@ public class JpaMain {
             //회원 저장
             Member member=new Member();
             member.setUserName("Member1");
-            // member.setTeamId(team.getId());
             member.setTeam(team);
             em.persist(member);
 
+            team.getMembers().add(member); // 편의 메소드로 생략 가능
+
             em.flush();
             em.clear();
-            //조회
-            Member findMember = em.find(Member.class, member.getId());
-            List<Member> members = findMember.getTeam().getMembers();
-            System.out.println("===========");
-            for (Member member1 : members) {
-                System.out.println(member1.getUserName());
-            }
 
+            Team findTeam = em.find(Team.class, team.getId());
+            List<Member> members = findTeam.getMembers();
+            //이시점에 쿼리가 또 날라감 member 쿼리 fc 없으면 안됨
+            //왜냐하면 영속성 컨텟흐트에 1차캐시로 저들이 저장되는데 members의 대한 정보가 없잔슴
+            //양방향일때는 양쪽에 다 세팅을 하자.
+
+            for (Member m : members) {
+                System.out.println(m.getUserName());
+            }
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
