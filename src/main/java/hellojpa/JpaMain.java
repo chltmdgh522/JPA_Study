4eccf44ac5f325e1c51e7eb9adfc237431ff4c07
seung_ho_choi.s <chltmdgh517@naver.com>
2024-02-23 18:10:18 +0900

    단방향 연관관계

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,8 +1,9 @@
 package hellojpa;
 
+import hellojpa.section5.Member;
+import hellojpa.section5.Team;
 import jakarta.persistence.*;
 
-import java.util.List;
 
 public class JpaMain {
 
@@ -15,15 +16,28 @@ public class JpaMain {
         tx.begin();
 
         try {
+            //팀 저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            //회원 저장
             Member member=new Member();
-            member.setUsername("A");
-            member.setRoleType(RoleType.USER);
-
-
-
-            System.out.println("=============");
+            member.setUserName("Member1");
+            // member.setTeamId(team.getId());
+            member.setTeam(team);
             em.persist(member);
-            System.out.println("=============");
+
+            //조회
+            Member findMember = em.find(Member.class, member.getId());
+            // Team findTeamId = em.find(Team.class, team.getId());
+            // Team findTeam= em.find(Team.class, findTeamId);
+            Team findTeam = findMember.getTeam();
+
+            //수정
+            Team newTeam = em.find(Team.class, 100L); //100번이 있다 가정하고
+            findMember.setTeam(newTeam);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
