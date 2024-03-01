3908d9954fed89f0625f46447307e2310418bbfb
seung_ho_choi.s <chltmdgh517@naver.com>
2024-03-01 17:50:36 +0900

    합병

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class JpaMain {
 
+
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
