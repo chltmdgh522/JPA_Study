2746cf0608dfa191a350b69d15bb8e0172b3b614
seung_ho_choi.s <chltmdgh517@naver.com>
2024-03-03 17:59:50 +0900

    값 타입과 불변 객체

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,11 +18,22 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Address address=new Address("서울","경리단길","1000");
             Member member=new Member();
             member.setUsername("최승호");
-            member.setHoneAddress(new Address("서울","경리단길","1000"));
+            member.setHoneAddress(address);
             member.setWordPeriod(new Period());
             em.persist(member);
+
+            Address copy=new Address(address.getCity(), address.getStreet(), address.getZipcode());
+
+            Member member2=new Member();
+            member2.setUsername("최승호");
+            member2.setHoneAddress(copy);
+            member2.setWordPeriod(new Period());
+            em.persist(member2);
+
+            member.getHoneAddress().setCity("인천"); //side effect 발생 set 제거나 프라이빗으로 하기
 
 
             tx.commit();
