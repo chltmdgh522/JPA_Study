8640603ccf1802751c1cdcb468e60063ee6a879a
seung_ho_choi.s <chltmdgh517@naver.com>
2024-03-08 19:07:49 +0900

    조건식

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -32,17 +32,24 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            //String query = "select m.userName, 'HELLO', TRUE from Member m "
-            //      + "where m.memberType=hellojpa.section10.MemberType.Admin";
-            String query = "select m.userName, 'HELLO', TRUE from Member m "
-                    + "where m.memberType=:userType";
-            List<Object[]> result = em.createQuery(query)
-                    .setParameter("userType",MemberType.User)
+            String query =
+                    "select " +
+                            "case when m.age <=10 then '학생요금' " +
+                            "when m.age >60 then '경로요금' " +
+                            "else '일반요금' " +
+                            " end"+
+                            " from Member m";
+
+            String query1="select coalesce(m.userName, '이름 없는 회원') from Member m";
+            String query2="select nullif(m.userName, '최승호') from Member m";
+
+            List<String> resultList = em.createQuery(query2, String.class)
                     .getResultList();
 
-            for (Object[] objects : result) {
-                System.out.println(objects[1]);
+            for (String s : resultList) {
+                System.out.println(s);
             }
+
 
             tx.commit();
         } catch (Exception e) {
