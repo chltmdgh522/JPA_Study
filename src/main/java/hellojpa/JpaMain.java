9664effa1e1187538445c5e70b6b7e257e2c4919
seung_ho_choi.s <chltmdgh517@naver.com>
2024-03-08 19:17:40 +0900

    jpql 함수

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -32,18 +32,9 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query =
-                    "select " +
-                            "case when m.age <=10 then '학생요금' " +
-                            "when m.age >60 then '경로요금' " +
-                            "else '일반요금' " +
-                            " end"+
-                            " from Member m";
+            String query = "select size(t.members) from Team t";
 
-            String query1="select coalesce(m.userName, '이름 없는 회원') from Member m";
-            String query2="select nullif(m.userName, '최승호') from Member m";
-
-            List<String> resultList = em.createQuery(query2, String.class)
+            List<String> resultList = em.createQuery(query, String.class)
                     .getResultList();
 
             for (String s : resultList) {
